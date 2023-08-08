d93659db2165f00f7dcbfbec4779a012d4e26524
SUA <haemahmtea@gmail.com>
2023-08-08 23:07:44 +0900

    index.hml setting

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`서버가 ${PORT}에서 실행 중입니다!`);
+});
