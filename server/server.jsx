52c6822370d66332fd60416944b267293147f1d4
haema <haemahmtea@gmail.com>
2023-08-21 12:37:29 +0900

    port set

--- server/server.jsx
+++ server/server.jsx
@@ -5,6 +5,8 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
+const PORT = process.env.PORT || 3000;
+
 app.prepare().then(() => {
     const server = express();
 
@@ -13,9 +15,9 @@ app.prepare().then(() => {
         return handle(req, res);
     });
 
-    server.listen(3000, (err) => {
+    server.listen(PORT, (err) => {
         if (err) throw err;
-        console.log('> Ready on http://localhost:3000');
+        console.log(`> Ready on http://localhost:${PORT}`);
     });
 });
 
